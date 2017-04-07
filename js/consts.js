2d145778c858efd5013981f9b423948ab2dd53d3
Gabor Heja <gheja@m00box.herd>
2017-04-07 17:51:17 +0200

    Added XP definitions and weapon classes

--- js/consts.js
+++ js/consts.js
@@ -8,7 +8,21 @@
 
 var LOCAL_STORAGE_PREFIX = "a";
 var FONT_NAME = "dunerevengmedium";
+var CHARACTER_LEVELS = [
+	0,
+	150 * Math.pow(2, 0),
+	150 * Math.pow(2, 1),
+	150 * Math.pow(2, 2),
+	150 * Math.pow(2, 3),
+	150 * Math.pow(2, 4),
+	150 * Math.pow(2, 5),
+	150 * Math.pow(2, 6),
+	150 * Math.pow(2, 7),
+	150 * Math.pow(2, 8)
+];
 
+var WEAPON_CLASS_ONE_HANDED = 1;
+var WEAPON_CLASS_TWO_HANDED = 2;
 var PLACES = [
 	{
 		"name": "",
