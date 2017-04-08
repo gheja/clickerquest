435175dad5a4e9caa10e9a5a7d21258381d2a01f
Gabor Heja <gheja@m00box.herd>
2017-04-08 21:00:40 +0200

    GfxLabel: added "inverted" property to draw with background color

--- js/gfxlabel.js
+++ js/gfxlabel.js
@@ -8,11 +8,19 @@ class GfxLabel extends GfxBase
 		
 		this.align = nvl(align, 'left');
 		this.text = nvl(text, 'label');
+		this.inverted = false;
 	}
 	
 	draw()
 	{
-		_gfx.finalCtx.fillStyle = _gfx.foreground;
+		if (!this.inverted)
+		{
+			_gfx.finalCtx.fillStyle = _gfx.foreground;
+		}
+		else
+		{
+			_gfx.finalCtx.fillStyle = _gfx.background;
+		}
 		_gfx.finalCtx.font = (16 * _gfx.z) + "px " + FONT_NAME;
 		_gfx.finalCtx.textAlign = this.align;
 		_gfx.finalCtx.fillText(this.text, Math.round(this.x * _gfx.z), this.y * _gfx.z);
