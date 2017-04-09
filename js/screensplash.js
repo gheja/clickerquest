6f14c9b602ecb2bf6b10cae36aad1fe72048d6d0
Gabor Heja <gheja@m00box.herd>
2017-04-09 20:18:06 +0200

    ScreenSplash: added epilepsy warning, moved objects to the objects array

--- js/screensplash.js
+++ js/screensplash.js
@@ -26,9 +26,13 @@ class ScreenSplash extends Screen2
 	
 	init()
 	{
-		this.cover = new GfxImage("cover_splash", 0, 32);
-		this.logo = new GfxImage("logo", 94, 32);
-		this.label = new GfxLabel(144, 180, "center", "Loading...");
+		this.objects['cover'] = new GfxImage("cover_splash", 0, 32);
+		this.objects['logo'] = new GfxImage("logo", 94, 32);
+		this.objects['label'] = new GfxLabel(144, 240, "center", "Loading...");
+		this.objects['label2'] = new GfxLabel(144, 160, "center", "WARNING");
+		this.objects['label3'] = new GfxLabel(144, 172, "center", "By default this game contains flashing images that might");
+		this.objects['label4'] = new GfxLabel(144, 180, "center", "trigger seizures for people with photosensitive epilepsy.");
+		this.objects['label5'] = new GfxLabel(144, 192, "center", "The effects can be turned off in the Options menu.");
 	}
 	
 	tick()
@@ -42,23 +46,14 @@ class ScreenSplash extends Screen2
 		{
 			if (this.ticks % 30 == 1)
 			{
-				this.label.text = "Click to continue"
+				this.objects['label'].text = "Click to continue"
 			}
 			else if (this.ticks % 30 == 16)
 			{
-				this.label.text = "";
+				this.objects['label'].text = "";
 			}
 		}
 		
-		this.logo.y = Math.floor(30 + Math.pow(Math.cos(_game.ticks * 0.03), 4) * 5);
-	}
-	
-	draw()
-	{
-		_gfx.clear();
-		
-		this.cover.draw();
-		this.logo.draw();
-		this.label.draw();
+		this.objects['logo'].y = Math.floor(30 + Math.pow(Math.cos(_game.ticks * 0.03), 4) * 5);
 	}
 }
