e3672ec17e1199d4fd088c91a985c4bf3a948549
Gabor Heja <gheja@m00box.herd>
2017-04-09 20:53:16 +0200

    ScreenSplash: modified warning to be in one label

--- js/screensplash.js
+++ js/screensplash.js
@@ -29,10 +29,12 @@ class ScreenSplash extends Screen2
 		this.objects['cover'] = new GfxImage("cover_splash", 0, 32);
 		this.objects['logo'] = new GfxImage("logo", 94, 32);
 		this.objects['label'] = new GfxLabel(144, 240, "center", "Loading...");
-		this.objects['label2'] = new GfxLabel(144, 160, "center", "WARNING");
-		this.objects['label3'] = new GfxLabel(144, 172, "center", "By default this game contains flashing images that might");
-		this.objects['label4'] = new GfxLabel(144, 180, "center", "trigger seizures for people with photosensitive epilepsy.");
-		this.objects['label5'] = new GfxLabel(144, 192, "center", "The effects can be turned off in the Options menu.");
+		this.objects['label2'] = new GfxLabel(144, 160, "center", "WARNING\n" +
+			"\n" +
+			"By default this game contains flashing images that might\n" +
+			"trigger seizures for people with photosensitive epilepsy.\n" +
+			"\n" +
+			"The effects can be turned off in the Options menu.");
 	}
 	
 	tick()
