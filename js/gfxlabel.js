05126c68b2f18c35a71b81721fc48863d97cac02
Gabor Heja <gheja@m00box.herd>
2017-04-17 17:22:43 +0200

    GfxLabel: border is now supported

--- js/gfxlabel.js
+++ js/gfxlabel.js
@@ -9,21 +9,14 @@ class GfxLabel extends GfxBase
 		this.align = nvl(align, 'left');
 		this.text = nvl(text, 'label');
 		this.inverted = false;
+		this.border = true;
 		this.scale = 1;
 	}
 	
 	draw()
 	{
-		let i, tmp;
+		let i, x, y, tmp;
 		
-		if (!this.inverted)
-		{
-			_gfx.finalCtx.fillStyle = _gfx.foreground;
-		}
-		else
-		{
-			_gfx.finalCtx.fillStyle = _gfx.background;
-		}
 		_gfx.finalCtx.font = (16 * _gfx.z * this.scale) + "px " + FONT_NAME;
 		_gfx.finalCtx.textAlign = this.align;
 		
@@ -34,6 +27,39 @@ class GfxLabel extends GfxBase
 		}
 		
 		tmp = this.text.split('\n');
+		
+		if (this.border)
+		{
+			if (this.inverted)
+			{
+				_gfx.finalCtx.fillStyle = _gfx.foreground;
+			}
+			else
+			{
+				_gfx.finalCtx.fillStyle = _gfx.background;
+			}
+			
+			for (x=-1; x<2; x++)
+			{
+				for (y=-1; y<2; y++)
+				{
+					for (i in tmp)
+					{
+						_gfx.finalCtx.fillText(tmp[i], Math.round((this.x + x) * _gfx.z), (this.y + y + i * 8 * this.scale) * _gfx.z);
+					}
+				}
+			}
+		}
+		
+		if (!this.inverted)
+		{
+			_gfx.finalCtx.fillStyle = _gfx.foreground;
+		}
+		else
+		{
+			_gfx.finalCtx.fillStyle = _gfx.background;
+		}
+		
 		for (i in tmp)
 		{
 			_gfx.finalCtx.fillText(tmp[i], Math.round(this.x * _gfx.z), (this.y + i * 8 * this.scale) * _gfx.z);
